5dea36c4ed27a48da79804b886f659a478bab3c0
71002-cloud <71002@edu.campusvejle.dk>
2026-06-16 10:40:11 +0200

    Now uses websocket

--- backend/worker/server.js
+++ backend/worker/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+global.WebSocket = require("ws");
 const { createClient} = require("@supabase/supabase-js");
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
